ce78013e5b9dc46caa31cb39b27a6ae876fa7565
Rahul rathore <rathorrahul0112@gmail.com>
2024-12-21 11:58:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import {useState} from 'react'
+import Test from "./Test";
+function App() {
+    const username = {name: 'kunal',age: 38}
+    let [counter, setCounter] = useState(15)
+
+    // let counter = 15;
+    const addValue = () =>{
+      console.log("click", counter )
+      counter = counter + 1;
+      setCounter(counter)
+    }
+    const removeVal = () =>{
+      setCounter(counter-1)
+      console.log(counter)
+    }
+  return (
+    <>
+    <Test />
+    <ul>
+      {Object.entries(username).map(([key, value]) =>(
+        <li key={key}>
+          {key}:{value}
+        </li>
+      ))}
+    </ul>
+    <h1>Hi I am React... </h1> 
+     <p>hi I am render </p>  
+     <h2>counter Value: {counter}</h2>
+
+     <button onClick={addValue}> Add value {counter}</button>
+     <br/>
+      <button onClick={removeVal}>remove button</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Test.js
@@ -0,0 +1,8 @@
+function Test(){
+    return(
+        <h1>
+            Hi.. coffee
+        </h1>
+    )
+}
+export default Test;
